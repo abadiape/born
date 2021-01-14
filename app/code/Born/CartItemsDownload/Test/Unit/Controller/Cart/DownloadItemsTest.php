50c996e8bcb924ef4a8e8734145a3a876989a0b2
Pedro Abadia <pedro.abadia@tlawanah.com>
2021-01-13 21:18:04 -0600

    PART4- Unit Testing 50%

---
+++ app/code/Born/CartItemsDownload/Test/Unit/Controller/Cart/DownloadItemsTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright 2020 © Born, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Born\CartItemsDownload\Test\Unit\Controller\Cart;
+
+/**
+ * DownloadItems ajax request
+ *
+ * @package Born\CartItemsDownload\Controller\Cart
+ */
+class DownloadItemsTest extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp()
+    {
+
+    }
+
+    /**
+     * Controller execute method
+     *
+     * @return void
+     */
+    public function testExecuteWithMessages()
+    {
+       
+
+    }
+}
